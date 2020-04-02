62b724258397d2f7c7b508481b1f253ffc7f21a9
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-04-02 16:54:49 +0700

    Change syntax checker to c++

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -17,14 +17,14 @@ flags = [
     # language to use when compiling headers. So it will guess. Badly. So C++
     # headers will be compiled as C headers. You don't want that so ALWAYS specify
     # a "-std=<something>".
-    # For a c++ project, you would set this to something like 'c++11' instead of
-    # 'c99'.
-    '-std=c99',
+    # For a c project, you would set this to something like 'c99' instead of
+    # 'c++11'.
+    '-std=c++11',
     # ...and the same thing goes for the magic -x option which specifies the
     # language that the files to be compiled are written in. This is mostly
     # relevant for c headers.
-    # For a c++ project, you would set this to 'c++' instead of 'c'.
-    '-x', 'c',
+    # For a c project, you would set this to 'c' instead of 'c++'.
+    '-x', 'c++',
     # This path will only work on OS X, but extra paths that don't exist are not
     # harmful
     '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
