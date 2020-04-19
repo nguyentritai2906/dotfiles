84555b09993df4f4b8feb2ad8e866f420495b8b1
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-04-19 17:15:51 +0700

    Scrot now overwrite destination, remove comments

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scrot /tmp/screen.png
+scrot -o /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
 if [[ -f $HOME/Pictures/icons/lock-green-20.png ]]
@@ -28,19 +28,4 @@ then
         echo "done"
     done
 fi
-# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-# i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
 i3lock -e -u -n -i /tmp/screen.png
-rm /tmp/screen.png
-
-##!/usr/bin/env bash
-#icon="$HOME/Pictures/icons/lock-green-20.png" tmpbg='/tmp/screen.png'
-#(( $# )) && { icon=$1; }
-#scrot "$tmpbg" convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg" convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg" i3lock -u -i "$tmpbg"
-##dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-#rm /tmp/screen.png
-
-##!/usr/bin/env bash
-#icon="$HOME/Pictures/icons/lock-green-20.png" tmpbg="/tmp/screen.png"
-#(( $# )) && { icon=$1; }
-#scrot "$tmpbg" convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg" convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg" i3lock -u -i "$tmpbg"
