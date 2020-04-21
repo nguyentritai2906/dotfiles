f449c2f2813cacaf9c352a80c19d576ffb090380
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-04-21 12:10:12 +0700

    Add volume control script for both Master and Bluetooth

---
+++ .config/i3/volume.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SINK=$(pacmd list-sinks | grep "index:" | grep '*' | awk '{print $3}')
+
+if [ "$SINK" == "" ]; then
+  SINK="0"
+fi
+
+if [ "$1" != "M" ]; then
+  pactl set-sink-volume "$SINK" "$1"  && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+else
+  pactl set-sink-mute "$SINK" toggle  && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+fi
