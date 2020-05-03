53855461fbe7a1189e84f1ef01b528cd5a313101
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-05-03 11:45:01 +0700

    Fix conditions

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -2,30 +2,24 @@
 
 BAR_ICON=""
 
-get_total_updates() { UPDATES=$(eopkg lu 2>/dev/null | wc -l) -1; }
+get_total_updates() { UPDATES=$(eopkg lu 2>/dev/null | wc -l) ; }
 
 while true; do
     get_total_updates
 
     # when there are updates available
-    # every 10 seconds another check for updates is done
-    while (( UPDATES > 0 )); do
-        if (( UPDATES == 1 )); then
-            echo "$BAR_ICON $UPDATES"
-        elif (( UPDATES > 1 )); then
-            echo "$BAR_ICON $UPDATES"
-        else
-            echo $BAR_ICON
-        fi
-        sleep 10
+    # every 30 mins another check for updates is done
+    while (( UPDATES > 1 )); do
+		echo "$BAR_ICON $UPDATES"
+        sleep 1800
         get_total_updates
     done
 
     # when no updates are available, use a longer loop, this saves on CPU
-    # and network uptime, only checking once every 30 min for new updates
-    while (( UPDATES == 0 )); do
+    # and network uptime, only checking once every hour for new updates
+    while (( UPDATES == 1 )); do
         echo $BAR_ICON 0
-        sleep 1800
+        sleep 3600
         get_total_updates
     done
 done
