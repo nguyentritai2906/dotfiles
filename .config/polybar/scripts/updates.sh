20372b5176b03db665d8256ee98283602a83c3ba
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-05-03 12:31:22 +0700

    If UPDATE=1 check content of output string

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -18,8 +18,14 @@ while true; do
     # when no updates are available, use a longer loop, this saves on CPU
     # and network uptime, only checking once every hour for new updates
     while (( UPDATES == 1 )); do
-        echo $BAR_ICON 0
-        sleep 3600
-        get_total_updates
+		if [ "$(eopkg lu 2>/dev/null)" == "No packages to upgrade." ]; then
+			echo "$BAR_ICON 0"
+			sleep 3600
+			get_total_updates
+		else
+			echo "$BAR_ICON $UPDATES"
+			sleep 1800
+			get_total_updates
+		fi
     done
 done
