179059a970afb9d124d1bc3e76b440be033f4343
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-05-10 11:13:36 +0700

    fzf.zsh

---
+++ .fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/solus/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/solus/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/solus/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "$HOME/.fzf/shell/key-bindings.zsh"
+source "$HOME/fzf.sh"
