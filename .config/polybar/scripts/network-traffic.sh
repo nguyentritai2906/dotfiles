432bcf5369c7c67691ab5247347e011f0dfd2764
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-05-31 21:24:36 +0700

    x

---
+++ .config/polybar/scripts/network-traffic.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Module showing network traffic. Shows how much data has been received (RX) or
+# transmitted (TX) since the previous time this script ran. So if run every
+# second, gives network traffic per second.
+
+logfile="${XDG_CACHE_HOME:-$HOME/.cache}/netlog"
+[ -f "$logfile" ] || echo "0 0" > "$logfile"
+read -r rxprev txprev < "$logfile"
+
+rxcurrent="$(($(paste -d '+' /sys/class/net/[ew]*/statistics/rx_bytes)))"
+txcurrent="$(($(paste -d '+' /sys/class/net/[ew]*/statistics/tx_bytes)))"
+
+up=$(((rxcurrent-rxprev)/1024))
+down=$(((txcurrent-txprev)/1024))
+
+if (( $up > 1024 || $down > 1024 )); then
+    printf " %dMiB   %dMiB" "$(($up/1024))" "$(($down/1024))"
+else
+    printf " %dKiB   %dKiB" "$up" "$down"
+fi
+
+echo "$rxcurrent $txcurrent" > "$logfile"
