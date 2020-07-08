3283c87832e6a6c038a43336b5e579d27bfabee9
nguyentritai2906 <nguyentritai2906@gmail.com>
2020-07-08 09:46:32 +0700

    x

--- .config/i3/volume.sh
+++ .config/i3/volume.sh
@@ -7,7 +7,9 @@ if [ "$SINK" == "" ]; then
 fi
 
 if [ "$1" != "M" ]; then
-  pactl set-sink-volume "$SINK" "$1"  && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+  pactl set-sink-volume "$SINK" "$1"
+  #pactl set-sink-volume "$SINK" "$1"  && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
 else
-  pactl set-sink-mute "$SINK" toggle  && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+  pactl set-sink-mute "$SINK" toggle
+  #pactl set-sink-mute "$SINK" toggle  && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
 fi
