783a38e905d8ddfecee2462dc46af36724739d3e
natri23 <natri23@gmail.com>
2020-11-20 22:28:10 +0700

    Environment variables for ibus

---
+++ .config/ibus-bamboo/ibus.sh
@@ -0,0 +1,7 @@
+# Copy to /usr/share/defaults/etc/profile.d
+# Logout
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT4_IM_MODULE=ibus
+export CLUTTER_IM_MODULE=ibus
