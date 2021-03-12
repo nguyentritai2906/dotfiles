071dfdb80b0c6924d6d37b8d8434d44e2bd8a6c6
nguyentritai2906 <nguyentritai2906@gmail.com>
2021-03-12 11:29:15 +0700

    Nvim Tree Sitter

--- .config/nvim/nvim-treesitter.lua
+++ .config/nvim/nvim-treesitter.lua
@@ -1,28 +1,28 @@
 lua <<EOF
 require'nvim-treesitter.configs'.setup {
-        ensure_installed = "maintained",
-        highlight = {
-            enable = true,
-            custom_captures = {
-                -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-                ["foo.bar"] = "Identifier",
-            },
+    ensure_installed = "maintained",
+    highlight = {
+        enable = true,
+        custom_captures = {
+            -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+            ["foo.bar"] = "Identifier",
         },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "+",
-                node_incremental = "+",
-                scope_incremental = "+",
-                node_decremental = "_",
-            },
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "+",
+            scope_incremental = "+",
+            node_incremental = "+",
+            node_decremental = "_",
         },
-        indent = {
-            enable = true
-        },
-        rainbow = {
+    },
+    indent = {
+        enable = true
+    },
+    rainbow = {
         enable = true,
         disable = {'bash'} -- please disable bash until I figure #1 out
-        }
     }
+}
 EOF
