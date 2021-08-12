4c256058295e81adf7dcd68b4c2380b4d6868c20
nguyentritai2906 <nguyentritai2906@gmail.com>
2021-08-12 22:10:32 +0700

    Fix vimls cause 'format got nil'

--- .config/nvim/lua/nv-lsp/init.lua
+++ .config/nvim/lua/nv-lsp/init.lua
@@ -53,7 +53,8 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {"vimls"}
+-- local servers = {"vimls"} -- Somehow this cause handler.lua error 'format expected number, got nil'
+local servers = {}
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { on_attach = on_attach }
 end
