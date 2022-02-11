eb42f49ee4ac0001415fc3088b2c22c2abf2b5c0
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2022-02-11 13:30:31 +0700

    temporary disable indent for python

--- .config/nvim/lua/modules/treesitter.lua
+++ .config/nvim/lua/modules/treesitter.lua
@@ -10,7 +10,8 @@ require'nvim-treesitter.configs'.setup {
         },
     },
     indent = {
-        enable = true
+        enable = true,
+        disable = {"python"}
     },
     rainbow = {
         enable = true,
