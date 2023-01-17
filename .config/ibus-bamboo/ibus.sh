1f845fcd417f7d161e07fdd865b43eaab51e7773
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2023-01-17 21:46:42 +0700

    chore: clean up unused configs

--- .config/ibus-bamboo/ibus.sh
+++
@@ -1,7 +0,0 @@
-# Copy to /usr/share/defaults/etc/profile.d
-# Logout
-export GTK_IM_MODULE=ibus
-export QT_IM_MODULE=ibus
-export XMODIFIERS=@im=ibus
-export QT4_IM_MODULE=ibus
-export CLUTTER_IM_MODULE=ibus
